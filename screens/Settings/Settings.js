18198328dd4163e7a44a7d38a995e0647cfb7b33
jameswahome <jameswahome54@gmail.com>
2021-10-28 13:25:40 +0300

    updates

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -5,6 +5,8 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  Image,
+  Platform,
 } from "react-native";
 import {
   LIGHT_COLOR,
@@ -14,17 +16,64 @@ import {
 import { Feather as Icon, Entypo } from "@expo/vector-icons";
 import ProfileCard from "../../components/ProfileCard";
 import { UserContext } from "../../context/UserContext";
+import { launchImageLibrary } from "react-native-image-picker";
 
 const { width: wWidth, height: wHeight } = Dimensions.get("window");
 
+const createFormData = (photo, body = {}) => {
+  const data = new FormData();
+
+  data.append("photo", {
+    name: photo.fileName,
+    type: photo.type,
+    uri: Platform.OS === "ios" ? photo.uri.replace("file://", "") : photo.uri,
+  });
+
+  Object.keys(body).forEach((key) => {
+    data.append(key, body[key]);
+  });
+
+  return data;
+};
+
 const Settings = () => {
   const userDetails = useContext(UserContext);
+  const [photo, setPhoto] = React.useState(null);
+
+  const handleChoosePhoto = () => {
+    launchImageLibrary({ noData: true }, (response) => {
+      console.log(response);
+      if (response) {
+        setPhoto(response);
+      }
+    });
+  };
+
+  const handleUploadPhoto = () => {
+    fetch(`https://apimarketpalace.com/upload`, {
+      method: "POST",
+      body: createFormData(photo, { userId: "123" }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   return (
     <View style={styles.root}>
-      <TouchableOpacity onPress={() => console.log("will upload an image")}>
+      <TouchableOpacity onPress={() => console.log("this will upload image")}>
         <View style={styles.profileCardContainer}>
           <View style={styles.avatar}>
+            {photo && (
+              <Image
+                source={{ uri: photo.uri }}
+                style={{ width: 30, height: 300 }}
+              />
+            )}
             <View style={styles.plusContainer}>
               <Icon name="plus" size={20} color={LIGHT_COLOR} />
             </View>
